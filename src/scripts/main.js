5f3926f1014309467df098407cfa00e001f9118d
Alonso Mendez <alonsomendez@gmail.com>
2015-01-24 00:31:01 -0600

    Update map size.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,4 +12,16 @@ game.state.add('menu', require('./states/menu'));
 game.state.add('play', require('./states/play'));
 game.state.start('boot');
 
+game.updateMapSize = function() {
+  var width = window.innerWidth,
+    height = window.innerHeight;
+
+  game.height = height;
+  game.width = width;
+  game.renderer.resize(width, height);
+  game.camera.setSize(width, height);
+};
+
+window.onresize = game.updateMapSize;
+
 console.log('%c We are the %c#gamesRum ', 'background: #222; color: #aabb55', 'background: #222; color: #aaffee');
