1fc3020e09712ebe8aeede6b8b6f0b4e80cd0073
Alonso Mendez <alonsomendez@gmail.com>
2015-01-24 16:22:37 -0600

    Fix sprites routes for mob and npcs

--- src/scripts/states/preloader.js
+++ src/scripts/states/preloader.js
@@ -9,8 +9,8 @@ Preloader.prototype.constructor = Preloader;
 Preloader.prototype.preload = function() {
   this.game.load.spritesheet('tiles', 'img/telles0808_rpg_maker_tileset.png', 32, 32, 16*27);
   this.game.load.spritesheet('avatar', 'img/2p_russia_sprite_rpg_by_yumehoshichan-d675dxi.png', 32, 32, 4*6);
-  this.game.load.spritesheet('mobs', 'img/RE3___Monster_Sprites_v1_0_by_DoubleLeggy', 32, 32, 12*8);
-  this.game.load.spritesheet('npcs', 'img/Visoes_3_Sprites_by_DoubleLeggy', 32, 32, 12*8);
+  this.game.load.spritesheet('mobs', 'img/RE3___Monster_Sprites_v1_0_by_DoubleLeggy.png', 32, 32, 12*8);
+  this.game.load.spritesheet('npcs', 'img/Visoes_3_Sprites_by_DoubleLeggy.png', 32, 32, 12*8);
 };
 
 Preloader.prototype.create = function() {
