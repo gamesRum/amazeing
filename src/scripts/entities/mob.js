35e568803fb043e1197e08272ab4a42d35bb240a
Alonso Mendez <alonsomendez@gmail.com>
2015-01-24 07:49:28 -0600

    Mob collision provokes player to be damaged

--- src/scripts/entities/mob.js
+++ src/scripts/entities/mob.js
@@ -2,7 +2,7 @@
 
 var Being = require('./being');
 
-var Mob = module.exports = function(x, y, money, level, bounds, sprite, walkable) {
+var Mob = module.exports = function(id, game, player, x, y, money, level, bounds, sprite, entities, walkable) {
   Being.call(this);
   this.stats.money = money;
   this.stats.level = level;
@@ -12,6 +12,10 @@ var Mob = module.exports = function(x, y, money, level, bounds, sprite, walkable
   this.location.y = y;
   this.walkable = walkable;
   this.orientation = 0;
+  this.game = game;
+  this.entities = entities;
+  this.id = id;
+  this.player = player;
   this.chooseOrientation();
 };
 
@@ -26,8 +30,39 @@ Being.prototype.chooseOrientation = function() {
   this.orientation = Math.floor(Math.random() * 4);
 };
 
+Being.prototype.attack = function(entity) {
+  this.player.sprite.animations.play('attack');
+  this.player.moving = !this.player.moving;
+  entity.damage(this.stats.str);
+};
+
+Being.prototype.validCell = function(x, y) {
+  if(this.player.location.x === x && this.player.location.y === y) {
+    this.attack(this.player);
+    return false;
+  }
+
+  if(!this.walkable[x][y]) {
+    return false;
+  } else {
+    for (var index in this.entities) {
+      var mob = this.entities[index];
+      if(mob.id !== this.id) {
+        if (mob.location.x === x && mob.location.y === y) {
+          return false;
+        }
+      }
+    }
+  }
+
+  return true;
+};
+
 Being.prototype.chooseNextMove = function() {
-  var locationBackup = this.location;
+  var locationBackup = {
+    x: this.location.x,
+    y: this.location.y
+  };
 
   switch(this.orientation) {
     case 0:
@@ -59,9 +94,14 @@ Being.prototype.chooseNextMove = function() {
     this.location.y = this.bounds.y-1;
   }
 
-  if(this.walkable[this.location.x][this.location.y]) {
-    this.sprite.position.x = this.location.x * 32;
-    this.sprite.position.y = this.location.y * 32;
+  if(this.validCell(this.location.x, this.location.y)) {
+    this.game.add.tween(this.sprite).to(
+      {
+        x: this.location.x * 32,
+        y: this.location.y * 32
+      },
+      200, Phaser.Easing.linear, true
+    );
   } else {
     this.location = locationBackup;
     this.chooseOrientation();
