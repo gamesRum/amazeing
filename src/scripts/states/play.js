13a81ef7da35a1f825cbd2a01e41a62e46ef3cdf
Alonso Mendez <alonsomendez@gmail.com>
2015-01-24 02:29:44 -0600

    Refactoring, nicer movements on the avatar.

--- src/scripts/states/play.js
+++ src/scripts/states/play.js
@@ -7,6 +7,17 @@ var Play = module.exports = function() {
 Play.prototype = Object.create(Phaser.State.prototype);
 Play.prototype.constructor = Play;
 
+Play.prototype.player = {
+  sprite: null,
+  moving: false,
+  animating: false,
+  location: {
+    x: 0,
+    y: 0
+  },
+  timer: null
+};
+
 Play.prototype.drawMaze = function() {
   for (var i = 0; i < 64; i += 1) {
     for (var j = 0; j < 64; j += 1) {
@@ -31,79 +42,73 @@ Play.prototype.create = function() {
   this.walls.enableBody = true;
   this.walls.physicsBodyType = Phaser.Physics.ARCADE;
 
-  this.avatar = this.game.add.sprite(32, 32, 'avatar');
-  this.game.physics.enable(this.avatar);
-  this.game.camera.follow(this.avatar);
-  this.avatar.body.setSize(12, 16, 2, 0);
-  this.avatar.bringToTop();
-  this.avatar.position.x = 32;
-  this.avatar.position.y = 32;
+  this.player.sprite = this.game.add.sprite(32, 32, 'avatar');
+  this.game.physics.enable(this.player.sprite);
+  this.game.camera.follow(this.player.sprite);
+  this.player.sprite.body.setSize(12, 16, 2, 0);
+  this.player.sprite.bringToTop();
+  this.player.sprite.position.x = 0;
+  this.player.sprite.position.y = 0;
 
-  this.avatar.animations.add('stand', 0, 1, false);
-  this.avatar.animations.add('walk_left', 4, 10, true);
-  this.avatar.animations.add('walk_right', 8, 10, true);
-  this.avatar.animations.add('walk_up', 12, 10, true);
-  this.avatar.animations.add('walk_down', 16, 10, true);
-
-  this.player = {
-    inTween: false,
-    x: 0,
-    y: 0
-  };
+  this.player.sprite.animations.add('stand', [0], 1, false);
+  this.player.sprite.animations.add('walk_left', [4,5,6,7], 10, true);
+  this.player.sprite.animations.add('walk_right', [8,9,10,11], 10, true);
+  this.player.sprite.animations.add('walk_up', [12,13,14,15], 10, true);
+  this.player.sprite.animations.add('walk_down', [0,1,2,3], 10, true);
 
   this.drawMaze();
 
   this.cursors = this.game.input.keyboard.createCursorKeys();
 };
 
+Play.prototype.onComplete = function() {
+  this.player.moving = false;
+};
+
 Play.prototype.movePlayer = function(left, top) {
-  this.player.x += left;
-  this.player.y += top;
+  this.player.location.x += left;
+  this.player.location.y += top;
+  this.player.moving = true;
 
-  var player = this.player,
-      avatar = this.avatar;
+  this.game.add.tween(this.player.sprite).to(
+    {
+      x: this.player.location.x * 32,
+      y: this.player.location.y * 32
+    },
+    200, Phaser.Easing.Linear.None, true
+  ).onComplete.addOnce(this.onComplete, this);
 
-  if(player.inTween) {
-    console.log('noop!');
-  } else {
-
-    this.game.add.tween(this.avatar).to(
-      {
-        x: this.player.x * 32,
-        y: this.player.y * 32
-      },
-      1, Phaser.Easing.Linear.None, true
-    );
-
-    console.log('Location:', this.player.x, this.player.y, this.player.inTween);
-  }
+  this.game.physics.arcade.collide(this.player.sprite, self.walls);
 };
 
 Play.prototype.update = function() {
-  this.game.physics.arcade.collide(this.avatar, this.walls);
-  this.avatar.body.velocity.setTo(0, 0);
+  this.game.physics.arcade.collide(this.player.sprite, this.walls);
+  this.player.sprite.body.velocity.setTo(0, 0);
 
-  if(!this.player.inTween) {
+  if(!this.player.moving) {
+    this.player.sprite.animations.play('stand');
+
+    console.log('waiting key');
     this.game.input.update();
 
     if(this.cursors.down.isDown) {
       this.movePlayer(0,1);
-      //this.avatar.animations.play('walk_down');
+      this.player.sprite.animations.play('walk_down');
     }
 
     if(this.cursors.up.isDown) {
       this.movePlayer(0,-1);
-      //this.avatar.animations.play('walk_up');
+      this.player.sprite.animations.play('walk_up');
     }
 
     if(this.cursors.left.isDown) {
       this.movePlayer(-1, 0);
-      //this.avatar.animations.play('walk_left');
+      this.player.sprite.animations.play('walk_left');
     }
 
     if(this.cursors.right.isDown) {
       this.movePlayer(1, 0);
-      //this.avatar.animations.play('walk_right');
+      this.player.sprite.animations.play('walk_right');
     }
   }
 };
