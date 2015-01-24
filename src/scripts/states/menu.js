c067b88c6c060b923f1da0ecba24fb42612d0908
Alonso Mendez <alonsomendez@gmail.com>
2015-01-24 04:45:34 -0600

    Improved start screen

--- src/scripts/states/menu.js
+++ src/scripts/states/menu.js
@@ -7,13 +7,16 @@ Menu.prototype = Object.create(Phaser.State.prototype);
 Menu.prototype.constructor = Menu;
 
 Menu.prototype.create = function() {
+  this.background = this.game.add.tileSprite(0, 0, 1984, 1984, 'tiles', 8);
+  this.game.world.setBounds(0, 0, window.innerWidth, window.innerHeight);
+
   var titleStyle = {align: 'center', fill: '#ffffff', font: 'bold 45px Arial'};
   var title = this.add
     .text(this.world.centerX, 0, 'UberQuest', titleStyle)
     .anchor.setTo(0.5);
 
   this.add
-    .text(this.world.centerX, 150, 'Click to Start!', {fill: '#f00'})
+    .text(this.world.centerX, 150, 'Click to Start!', {fill: '#000000'})
     .anchor.set(0.5);
 
   this.add.tween(title)
