2fcafa529faf3429248ca28a9e796243f89205ef
Alonso Mendez <alonsomendez@gmail.com>
2015-01-24 01:38:43 -0600

    Keyboard bindings, need something to avoid player go crazy at the bounds xD

--- src/scripts/states/play.js
+++ src/scripts/states/play.js
@@ -36,14 +36,74 @@ Play.prototype.create = function() {
   this.game.camera.follow(this.avatar);
   this.avatar.body.setSize(12, 16, 2, 0);
   this.avatar.bringToTop();
+  this.avatar.position.x = 32;
+  this.avatar.position.y = 32;
+
+  this.avatar.animations.add('stand', 0, 1, false);
+  this.avatar.animations.add('walk_left', 4, 10, true);
+  this.avatar.animations.add('walk_right', 8, 10, true);
+  this.avatar.animations.add('walk_up', 12, 10, true);
+  this.avatar.animations.add('walk_down', 16, 10, true);
+
+  this.player = {
+    inTween: false,
+    x: 0,
+    y: 0
+  };
 
   this.drawMaze();
+
+  this.cursors = this.game.input.keyboard.createCursorKeys();
+};
+
+Play.prototype.movePlayer = function(left, top) {
+  this.player.x += left;
+  this.player.y += top;
+
+  var player = this.player,
+      avatar = this.avatar;
+
+  if(player.inTween) {
+    console.log('noop!');
+  } else {
+
+    this.game.add.tween(this.avatar).to(
+      {
+        x: this.player.x * 32,
+        y: this.player.y * 32
+      },
+      1, Phaser.Easing.Linear.None, true
+    );
+
+    console.log('Location:', this.player.x, this.player.y, this.player.inTween);
+  }
 };
 
 Play.prototype.update = function() {
   this.game.physics.arcade.collide(this.avatar, this.walls);
-};
+  this.avatar.body.velocity.setTo(0, 0);
 
-Play.prototype.switchBackToMenu = function() {
-  this.state.start('menu');
+  if(!this.player.inTween) {
+    this.game.input.update();
+
+    if(this.cursors.down.isDown) {
+      this.movePlayer(0,1);
+      //this.avatar.animations.play('walk_down');
+    }
+
+    if(this.cursors.up.isDown) {
+      this.movePlayer(0,-1);
+      //this.avatar.animations.play('walk_up');
+    }
+
+    if(this.cursors.left.isDown) {
+      this.movePlayer(-1, 0);
+      //this.avatar.animations.play('walk_left');
+    }
+
+    if(this.cursors.right.isDown) {
+      this.movePlayer(1, 0);
+      //this.avatar.animations.play('walk_right');
+    }
+  }
 };
