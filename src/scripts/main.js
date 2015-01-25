448dcec94ea38a3b3b3556c5ca5421be0aa34e70
Alonso Mendez <alonsomendez@gmail.com>
2015-01-25 16:42:58 -0600

    Menu removed, we don't need it anymore!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,7 +8,6 @@ var game = new Phaser.Game({
 
 game.state.add('boot', require('./states/boot'));
 game.state.add('preloader', require('./states/preloader'));
-game.state.add('menu', require('./states/menu'));
 game.state.add('play', require('./states/play'));
 game.state.start('boot');
 
