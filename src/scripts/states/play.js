f005b8e8cb269a85cb0cd3fdeeff1b168b187b25
Alonso Mendez <alonsomendez@gmail.com>
2015-01-25 03:21:33 -0600

    Die animation

--- src/scripts/states/play.js
+++ src/scripts/states/play.js
@@ -267,6 +267,7 @@ Play.prototype.loadMap = function (map) {
   this.player.sprite.animations.add('walk_down', [0, 1, 2, 3], 10, true);
   this.player.sprite.animations.add('damage', [16, 17, 18, 19], 10, true);
   this.player.sprite.animations.add('attack', [20, 21, 22, 23], 10, true);
+  this.player.sprite.animations.add('die', [24, 25, 26, 27], 1, false);
 
   this.drawMaze();
 };
@@ -438,6 +439,7 @@ Play.prototype.update = function () {
   }
 
   if (!this.player.isAlive()) {
+    this.player.sprite.animations.play('die');
     this.showMessage('GAME OVER!', {
       accept: function() {
         window.location.reload();
